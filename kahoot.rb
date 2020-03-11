b629c70b00918dfc4467a07e0c7aeb6344a7f1b5
Morgan <morgan.batterham@gmail.com>
2020-03-11 15:41:21 +1100

    day 2 arvo

--- kahoot.rb
+++ kahoot.rb
@@ -1,7 +1,9 @@
 require 'colorize'
 require 'cli/ui'
-require 'ffi'
+# require 'ffi'
 # require 'win32/sound'
+# require 'audio_player'
+
 require_relative './qanda.rb' 
 
 def kahoot(no_of_questions)
@@ -13,6 +15,9 @@ def kahoot(no_of_questions)
 	# This generates an array with the question numbers to pull from the question array.
 
 	while i < no_of_questions
+
+		system "clear"
+
 		current_q = q_bank[i]
 
 		# answer = CLI::UI::Prompt.ask(Q_ARR[current_q]) do |handler|
@@ -21,8 +26,7 @@ def kahoot(no_of_questions)
 		# 	handler.option(A_ARR[current_q][3])   { |selection| selection }
 		# 	handler.option(A_ARR[current_q][4]) { |selection| selection }
 		#   end
-
-		CLI::UI::Prompt.ask(Q_ARR[current_q], options: A_ARR[current_q][1..4])
+		answer = CLI::UI::Prompt.ask("Question #{i+1}:\n" + Q_ARR[current_q], options: A_ARR[current_q][1..4])
 
 		# puts Q_ARR[current_q]
 		# puts A_ARR[current_q][1..4]
@@ -33,18 +37,23 @@ def kahoot(no_of_questions)
 		# 	puts "Invalid answer. Please try again:"
 		# else
 		
-		correct_answer = A_ARR[current_q][0]
-			if answer == A_ARR[current_q][0] 
+		correct_answer_index = A_ARR[current_q][0]
+		correct_answer = A_ARR[current_q][correct_answer_index]
+			if answer == correct_answer 
 				# sound.play(correct.wav)
 				score += 1
 				puts "You have entered the correct answer! Your score is now #{score}".colorize(:green)
 			else 
 				# sound.play(incorrect.wav)
 				puts "Incorrect answer :( Your score is still #{score}".colorize(:red)
+				puts "The correct answer was '#{correct_answer}'"
 			end
 			i += 1
+			sleep (3)
 		# end
 	end
+
+	system "clear"
 
 	if score == no_of_questions
 		puts "Great work! You got all #{score} questions correct - 100%"
@@ -59,23 +68,41 @@ end
 
 play_game = true
 
+system "clear"
+
+puts "Welcome to KaHoot!"
+
+sleep(1)
+
 while play_game == true
 
 	puts "How many questions would you like to answer?"
 	print "Maximum of #{Q_ARR.length} questions: "
 	no_of_questions = gets.chomp.to_i
-	kahoot(no_of_questions)
+	if no_of_questions <= Q_ARR.length && no_of_questions > 0
+		kahoot(no_of_questions)
+	else
+		puts "Invalid selection"
+		invalid = true
+	end
 
-
-	print "Would you like to play again (y/n): "
-	play_game_answer = gets.chomp.downcase
-	play_game_answer = play_game_answer[0]
-		if play_game_answer == "y"
+	if invalid != true
+		# print "Would you like to play again (y/n): "
+		# play_game_answer = gets.chomp.downcase
+		# play_game_answer = play_game_answer[0]
+		# 	if play_game_answer == "y"
+		# 		play_game = true
+		# 	else
+		# 		play_game = false
+		# 	end
+		play_game_answer = CLI::UI::Prompt.ask("Would you like to play again?", options: %w(yes no))
+		if play_game_answer == "yes"
 			play_game = true
 		else
 			play_game = false
 		end
-			
+	else
+	end
 end
 
 
